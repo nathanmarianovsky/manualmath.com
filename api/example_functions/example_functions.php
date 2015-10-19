718366614c64e0947e1cf54434cd34b9fe636fec
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-10-19 02:24:08 -0400

    A Couple More Small Fixes

--- api/example_functions/example_functions.php
+++ api/example_functions/example_functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "api/classes/example.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/api/classes/example.php";
 
 /*
 
--- api/section_functions/section_functions.php
+++ api/section_functions/section_functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "api/classes/section.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/api/classes/section.php";
 
 /*
 
