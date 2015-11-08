e50a6a1df0ca9ebe2a3d48a3b67db9449abadcc3
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-11-08 18:04:10 -0500

    Easier Development Setup using Gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,22 @@
 // Defines all the required packages
 var gulp = require("gulp"),
 	install = require("gulp-install"),
-	gutil = require("gulp-util"),
 	exec = require("child_process").exec;
 
 // Runs bower install and npm install for the root directory
 gulp.task("root_packages", function() {
-	gulp.src(["./bower.json"]).pipe(install({"config.interactive": false, allowRoot: true}));
-	gutil.log("Bower Components Have Been Added!");
-	gulp.src(["./package.json"]).pipe(install());
-	gutil.log("Node Modules Have Been Added!");
+	return gulp.src(["./bower.json", "./package.json"]).pipe(install({"config.interactive": false, allowRoot: true}));
 });
 
 // Runs bower install and npm install for the MaterializeCSS-AMD library
-gulp.task("materialize_packages", function() {
-	gulp.src(["./bower_components/materializecss-amd/bower.json"]).pipe(install({"config.interactive": false, allowRoot: true}));
-	gutil.log("Bower Components for MaterializeCSS-AMD Have Been Added!");
-	gulp.src(["./bower_components/materializecss-amd/package.json"]).pipe(install());
-	gutil.log("Node Modules for MaterializeCSS-AMD Have Been Added!");
+gulp.task("materialize_packages", ["root_packages"], function() {
+	process.chdir("./bower_components/materializecss-amd");
+	return gulp.src(["./bower.json", "./package.json"]).pipe(install({"config.interactive": false, allowRoot: true}));
 });
 
 // Runs gulp build for the MaterializeCSS-AMD library
-gulp.task("build_materialize", function() {
-	process.chdir("./bower_components/materializecss-amd");
-	exec("gulp build");
-	gutil.log("Gulp Build Has Executed for MaterializeCSS-AMD");
+gulp.task("build_materialize", ["materialize_packages"], function() {
+	return exec("gulp build");
 });
+
+gulp.task("default", ["build_materialize"]);
