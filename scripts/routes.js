4ec9692acdb62f0f11c10092e54fa803bbc6db63
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-03-15 02:24:37 -0400

    Routes.js Update
    Switched the functions to lambda expressions to keep up with the times!

--- scripts/routes.js
+++ scripts/routes.js
@@ -19,16 +19,16 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 			An array containing all of the examples
 
 	*/
-	exports.add_listeners = function(router, subjects, topics, sections, examples) {
-		router.addRouteListener("about", function(toState, fromState) {
+	exports.add_listeners = (router, subjects, topics, sections, examples) => {
+		router.addRouteListener("about", (toState, fromState) => {
 			navs.subject_side_nav(subjects);
 			$(".page_title").text("About");
 			$("title").text("About");
 			$("main").empty();
 			$("main").append($("<div>").attr("id", "about_page"));
-			$.get("/client/about.php").done(function(content) {
+			$.get("/client/about.php").done(content => {
 				$("#about_page").append(content);
-				$.get("/client/notation.php").done(function(notation) {
+				$.get("/client/notation.php").done(notation => {
 					$("#notation_box").append(notation);
 					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"main"]);
 				});
@@ -37,8 +37,8 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 			links.handle_links(router, subjects, topics, sections, examples);
 		});
 
-		router.addRouteListener("subject", function(toState, fromState) {
-			var subject = subjects.filter(function(iter) {
+		router.addRouteListener("subject", (toState, fromState) => {
+			var subject = subjects.filter(iter => {
 				return iter.sname == toState.params.sname;
 			})[0];
 			navs.topic_side_nav(subject);
@@ -46,9 +46,9 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 			$(".page_title").text(subject.clean_name);
 			$("title").text(subject.clean_name);
 			$("main").append($("<div>").attr("id", "subject_page"));
-			$.get("/content/" + subject.sname + "/" + subject.sname + ".html").done(function(content) {
+			$.get("/content/" + subject.sname + "/" + subject.sname + ".html").done(content => {
 				$("#subject_page").append(content)
-				$.get("/content/" + subject.sname + "/Notation.html").done(function(notation) {
+				$.get("/content/" + subject.sname + "/Notation.html").done(notation => {
 					$("#subject_page").append(notation);
 					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"main"]);
 				});
@@ -59,11 +59,11 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 			links.handle_links(router, subjects, topics, sections, examples);
 		});
 
-		router.addRouteListener("subject.topic", function(toState, fromState) {
-			var subject = subjects.filter(function(iter) {
+		router.addRouteListener("subject.topic", (toState, fromState) => {
+			var subject = subjects.filter(iter => {
 				return iter.sname == toState.params.sname;
 			})[0],
-				topic = subject.topics.filter(function(iter) {
+				topic = subject.topics.filter(iter => {
 				return iter.tname == toState.params.tname;
 			})[0];
 			navs.section_side_nav(topic, subject);
@@ -71,7 +71,7 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 			$(".page_title").text(subject.clean_name + " - " + topic.clean_name);
 			$("title").text(subject.clean_name + " - " + topic.clean_name);
 			$("main").append($("<div>").attr("id", "topic_page"));
-			$.get("/content/" + subject.sname + "/" + topic.tname + "/" + topic.tname + ".html").done(function(content) {
+			$.get("/content/" + subject.sname + "/" + topic.tname + "/" + topic.tname + ".html").done(content => {
 				$("#topic_page").append(content);
 				MathJax.Hub.Queue(["Typeset",MathJax.Hub,"main"]);
 			});
@@ -81,17 +81,17 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 			links.handle_links(router, subjects, topics, sections, examples);
 		});
 
-		router.addRouteListener("subject.topic.section.current_page", function(toState, fromState) {
+		router.addRouteListener("subject.topic.section.current_page", (toState, fromState) => {
 			if(window.innerWidth < 992) {
 				$(".button-collapse").sideNav("hide");
 			}
-			var subject = subjects.filter(function(iter) {
+			var subject = subjects.filter(iter => {
 				return iter.sname == toState.params.sname;
 			})[0],
-				topic = subject.topics.filter(function(iter) {
+				topic = subject.topics.filter(iter => {
 				return iter.tname == toState.params.tname;
 			})[0],
-				section = topic.sections.filter(function(iter) {
+				section = topic.sections.filter(iter => {
 				return iter.section_name == toState.params.section_name;
 			})[0];
 			$("main").empty();
@@ -116,18 +116,18 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 
 			if(section.section_name == toState.params.current_page_name) {
 				$("#section_name" + section.section_id).addClass("active");
-				$.get("/content/" + subject.sname + "/" + topic.tname + "/" + section.section_name + "/" + section.section_name + ".html").done(function(content) {
+				$.get("/content/" + subject.sname + "/" + topic.tname + "/" + section.section_name + "/" + section.section_name + ".html").done(content => {
 					$("#latex").append(content);
 					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"main"]);
 					functions.handle_button("notes");
 				});
 			}
 			else {
-				var example = section.examples.filter(function(iter) {
+				var example = section.examples.filter(iter => {
 					return iter.ename == toState.params.current_page_name;
 				})[0];
 				$("#examples_li" + example.eid).addClass("active");
-				$.get("/content/" + subject.sname + "/" + topic.tname + "/" + section.section_name + "/" + example.ename + ".html").done(function(content) {
+				$.get("/content/" + subject.sname + "/" + topic.tname + "/" + section.section_name + "/" + example.ename + ".html").done(content => {
 					$("#latex").append(content);
 					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"main"]);
 					functions.handle_button("examples");
@@ -141,7 +141,7 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 
 
 
-			$(document).keydown(function(event) {
+			$(document).keydown(event => {
 				if(event.which == 37) {
 					event.preventDefault();
 					event.stopPropagation();
@@ -153,7 +153,7 @@ define(["app/functions", "app/navs", "app/links"], function(functions, navs, lin
 							router.navigate("subject.topic.section.current_page", {sname: subject.sname, tname: topic.tname, section_name: section.section_name, current_page_name: section.section_name});
 						}
 						else {
-							var next_example = section.examples.filter(function(iter) {
+							var next_example = section.examples.filter(iter => {
 								return iter.order == example.order - 1;
 							})[0];
 							router.navigate("subject.topic.section.current_page", {sname: subject.sname, tname: topic.tname, section_name: section.section_name, current_page_name: next_example.ename});
