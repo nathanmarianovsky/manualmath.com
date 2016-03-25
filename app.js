ddefe4abe9ddcf58b9f5e9a936e79d986c5258fd
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-03-25 01:30:52 -0400

    App.js
    Cleanup!

--- app.js
+++ app.js
@@ -4,57 +4,37 @@ var express = require("express"),
 	fs = require("fs"),
 	os = require("os"),
 	recursive = require("recursive-readdir"),
+	mkdirp = require("mkdirp"),
 	compressor = require("node-minify"),
+	compression = require("compression"),
 	minify = require("html-minifier").minify,
 	client_routes = require("./scripts/back-end/client_routes"),
 	api_routes = require("./scripts/back-end/api_routes"),
 	config = require("./scripts/back-end/config"),
-	container = [],
-	devpath = "./",
-	distpath = "./",
+	minifier = require("./scripts/back-end/minifier"),
 	app = express(),
 	pool = config.add_connections(mysql);
 
+// Tells the app use to compress files whenever possible
+app.use(compression());
 
 // Adds all of the routes
 client_routes.add_client_routes(app);
 api_routes.add_api_routes(app, pool);
 
+// Minifies all html files in /client, CSS filles in /styles/dev, and RequireJS
+minifier.minify_all_but_content(mkdirp, compressor, minify, fs);
 
-// Reads the directory of all the styles and minifies them all
-fs.readdir("./styles/dev", (err, files) => {
-	if(err) { console.log("Reading the directory hit an issue: " + err.stack); }
-	var wait = files.length;
-	files.forEach(file => {
-		var name = file.split("."),
-			devpath = "./styles/dev/" + file,
-			distpath = "./styles/dist/" + name[0] + "-min.css";
-		new compressor.minify({
-			"type": "yui-css",
-			"fileIn": devpath,
-			"fileOut": distpath,
-			"callback": (err, result) => {
-				if(err) { console.log("Minifying the css file " + file + " threw an error: " + err.stack); }
-			}
-		});
-		wait--;
-		if(wait == 0) {
-			// Minifies requirejs 
-			new compressor.minify({
-				"type": "yui-js",
-				"fileIn": "./node_modules/requirejs/require.js",
-				"fileOut": "./scripts/dist/require-min.js",
-				"callback": (err, result) => {
-					if(err) { console.log("Minifying the requirejs file threw an error: " + err.stack); }
-					// Tells the server to listen now that everything has been taken care of
-					app.listen(8080, () => {
-						console.log("The server is now listening!");
-					});
-				}
-			});
-		}
-	});
+// Tells the server to listen now that everything has been taken care of
+app.listen(8080, () => {
+	console.log("The server is now listening!");
 });
+
+
+
+
+
+
 
 
 
@@ -99,20 +79,3 @@ fs.readdir("./styles/dev", (err, files) => {
 
 
 
-// fs.readFile("./content/Ordinary_Differential_Equations/Notation.html", "utf8", (err, data) => {
-// 	if(err) { console.log("Could not read the file " + devpath + container[container.length - 1] + ": " + err.stack); }
-// 	var data_min = minify(data, {
-// 		"removeComments": true,
-// 		"removeCommentsFromCDATA": true,
-// 		"collapseWhitespace": true,
-// 		"collapseInlineTagWhitespace": true,
-// 		"removeAttributeQuotes": true,
-// 		"removeRedundantAttributes": true,
-// 		"useShortDoctype": true,
-// 		"minifyJS": true,
-// 		"minifyCSS": true
-// 	});
-// 	fs.writeFile("./content/Ordinary_Differential_Equations/Notation-min.html", data_min, err => {
-// 		if(err) { console.log("Could not write the file " + distpath + container[container.length - 1].split(".")[0] + "-min.html: " + err.stack); }
-// 	});
-// });
