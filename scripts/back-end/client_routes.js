4ea7b032dc28640c859baf277bd79867307408d0
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-03-27 22:17:55 -0400

    Fix on the Client Routes

--- scripts/back-end/client_routes.js
+++ scripts/back-end/client_routes.js
@@ -8,15 +8,9 @@ exports.add_client_routes = app => {
 	});
 
 
-	// This will load the template file where the rest is handled by the front-end
-	app.get("/client/about", (request, response) => {
-		response.sendFile("./client/dist/template-min.html", { "root": "./" });
-	});
-
-
-	// Any url going to the client folder will automatically all redirect to /client/about
+	// All client requests
 	app.get("/client/*", (request, response) => {
-		response.redirect("/client/about");
+		response.sendFile("./client/dist/template-min.html", { "root": "./" });
 	});
 };
 
