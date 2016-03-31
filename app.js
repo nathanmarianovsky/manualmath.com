396bc70af7fbe261d4f98e9fcb43a6cdff8c230b
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-03-30 20:40:40 -0400

    Separated Minification Process

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var express = require("express"),
 	client_routes = require("./scripts/back-end/client_routes"),
 	api_routes = require("./scripts/back-end/api_routes"),
 	config = require("./scripts/back-end/config"),
+	minifier = require("./scripts/back-end/minifier"),
 	app = express(),
 	pool = config.add_connections(mysql),
 	container = [],
@@ -30,89 +31,15 @@ app.use(compression());
 app.use(favicon("./favicon.ico", {"maxAge": 2592000000 }));
 
 // Tells the app to use the current directory as the default path
-// app.use(express.static(__dirname, {"maxAge": 864000000 }));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, {"maxAge": 864000000 }));
 
 // Adds all of the routes
 client_routes.add_client_routes(app);
 api_routes.add_api_routes(app, pool, fs);
 
-// Reads the directory of all the styles and minifies them all
-fs.readdir("./styles/dev", (err, files) => {
-	if(err) { console.log("Reading the directory hit an issue: " + err.stack); }
-	wait = files.length;
-	files.forEach(file => {
-		var name = file.split("."),
-			devpath = "./styles/dev/" + file,
-			distpath = "./styles/dist/" + name[0] + "-min.css";
-		new compressor.minify({
-			"type": "yui-css",
-			"fileIn": devpath,
-			"fileOut": distpath,
-			"callback": (err, result) => {
-				if(err) { console.log("Minifying the css file " + file + " threw an error: " + err.stack); }
-			}
-		});
-		wait--;
-		if(wait == 0) {
-			// Minifies requirejs 
-			new compressor.minify({
-				"type": "yui-js",
-				"fileIn": "./node_modules/requirejs/require.js",
-				"fileOut": "./scripts/dist/require-min.js",
-				"callback": (err, result) => {
-					if(err) { console.log("Minifying the requirejs file threw an error: " + err.stack); }
-					// Minifies the html files in /client
-					container = ["./client/template.html", "./client/about.html", "./client/notation.html"];
-					container.forEach(file => {
-						fs.readFile(file, "utf8", (err, data) => {
-							if(err) { console.log("Could not read the file " + file + ": " + err.stack); }
-							splitting = file.split("/");
-							newpath = "";
-							name = splitting[splitting.length - 1].split(".");
-							for(var k = 0; k < splitting.length - 1; k++) {
-								newpath += splitting[k] + "/";
-							}
-							newpath += "dist/";
-							var data_min = minify(data, {
-								"removeComments": true,
-								"removeCommentsFromCDATA": true,
-								"collapseWhitespace": true,
-								"collapseInlineTagWhitespace": true,
-								"removeAttributeQuotes": true,
-								"removeRedundantAttributes": true,
-								"useShortDoctype": true,
-								"minifyJS": true,
-								"minifyCSS": true
-							});
-							var obj = {
-								"data_min": data_min,
-								"file_name": newpath + name[0] + "-min." + name[1],
-							};
-							tmp.push(obj);
-							if(tmp.length == container.length) {
-								wait = container.length;
-								mkdirp(newpath, err => {
-									if(err) { console.log("Could not make the directory" + newpath + ": " + err.stack); }
-									tmp.forEach(result => {
-										fs.writeFile(result.file_name, result.data_min, err => {
-											if(err) { console.log("Could not write the file " + result.file_name + ": " + err.stack); }
-											wait--;
-											if(wait == 0) {
-												console.log("All html files in /client, CSS files in /styles/dev, and RequireJS have been minified!");
-												// Tells the server to listen now that everything has been taken care of
-												app.listen(80, () => {
-													console.log("The server is now listening!");
-												});
-											}
-										});
-									});
-								});
-							}
-						});
-					});
-				}
-			});
-		}
+minifier.driver(app, mkdirp, compressor, minify, fs, () => {
+	// Tells the server to listen
+	app.listen(80, () => {
+		console.log("The server is now listening!");
 	});
 });
--- scripts/back-end/minifier.js
+++ scripts/back-end/minifier.js
@@ -1,20 +1,108 @@
 var exports = {};
 
-// Minifies all of the CSS files, RequireJS, and html files found inside of /client
-exports.minify_all_but_content = (mkdirp, compressor, minify, fs) => {
-	var container = [],
-		splitting = [],
-		name = [],
-		tmp = [],
-		devpath = "./",
-		distpath = "./",
-		newpath = "./",
-		wait = 0;
+// // Minifies all of the CSS files, RequireJS, and html files found inside of /client
+// exports.minify_all_but_content = (mkdirp, compressor, minify, fs) => {
+// 	var container = [],
+// 		splitting = [],
+// 		name = [],
+// 		tmp = [],
+// 		devpath = "./",
+// 		distpath = "./",
+// 		newpath = "./",
+// 		wait = 0;
 
-	// Reads the directory of all the styles and minifies them all
+// 	// Reads the directory of all the styles and minifies them all
+// 	fs.readdir("./styles/dev", (err, files) => {
+// 		if(err) { console.log("Reading the directory hit an issue: " + err.stack); }
+// 		wait = files.length;
+// 		files.forEach(file => {
+// 			var name = file.split("."),
+// 				devpath = "./styles/dev/" + file,
+// 				distpath = "./styles/dist/" + name[0] + "-min.css";
+// 			new compressor.minify({
+// 				"type": "yui-css",
+// 				"fileIn": devpath,
+// 				"fileOut": distpath,
+// 				"callback": (err, result) => {
+// 					if(err) { console.log("Minifying the css file " + file + " threw an error: " + err.stack); }
+// 				}
+// 			});
+// 			wait--;
+// 			if(wait == 0) {
+// 				// Minifies requirejs 
+// 				new compressor.minify({
+// 					"type": "yui-js",
+// 					"fileIn": "./node_modules/requirejs/require.js",
+// 					"fileOut": "./scripts/dist/require-min.js",
+// 					"callback": (err, result) => {
+// 						if(err) { console.log("Minifying the requirejs file threw an error: " + err.stack); }
+// 						// Minifies the html files in /client
+// 						container = ["./client/template.html", "./client/about.html", "./client/notation.html"];
+// 						container.forEach(file => {
+// 							fs.readFile(file, "utf8", (err, data) => {
+// 								if(err) { console.log("Could not read the file " + file + ": " + err.stack); }
+// 								splitting = file.split("/");
+// 								newpath = "";
+// 								name = splitting[splitting.length - 1].split(".");
+// 								for(var k = 0; k < splitting.length - 1; k++) {
+// 									newpath += splitting[k] + "/";
+// 								}
+// 								newpath += "dist/";
+// 								var data_min = minify(data, {
+// 									"removeComments": true,
+// 									"removeCommentsFromCDATA": true,
+// 									"collapseWhitespace": true,
+// 									"collapseInlineTagWhitespace": true,
+// 									"removeAttributeQuotes": true,
+// 									"removeRedundantAttributes": true,
+// 									"useShortDoctype": true,
+// 									"minifyJS": true,
+// 									"minifyCSS": true
+// 								});
+// 								var obj = {
+// 									"data_min": data_min,
+// 									"file_name": newpath + name[0] + "-min." + name[1],
+// 								};
+// 								tmp.push(obj);
+// 								if(tmp.length == container.length) {
+// 									wait = container.length;
+// 									mkdirp(newpath, err => {
+// 										if(err) { console.log("Could not make the directory" + newpath + ": " + err.stack); }
+// 										tmp.forEach(result => {
+// 											fs.writeFile(result.file_name, result.data_min, err => {
+// 												if(err) { console.log("Could not write the file " + result.file_name + ": " + err.stack); }
+// 												wait--;
+// 												if(wait == 0) {
+// 													console.log("All html files in /client, CSS files in /styles/dev, and RequireJS have been minified!");
+// 												}
+// 											});
+// 										});
+// 									});
+// 								}
+// 							});
+// 						});
+// 					}
+// 				});
+// 			}
+// 		});
+// 	});
+// }
+
+// Driver function to handle all of the minification
+exports.driver = (app, mkdirp, compressor, minify, fs, callback) => {
+	// exports.minify_styles(compressor, fs, exports.minify_js(compressor, fs, exports.minify_html(minify, mkdirp, fs, () => {
+	// })));
+	exports.minify_styles(compressor, fs);
+	exports.minify_js(compressor, fs);
+	exports.minify_html(minify, mkdirp, fs);
+	console.log("Everything has been minified!");
+	callback();
+};
+
+// Reads the directory of all the styles and minifies them all
+exports.minify_styles = (compressor, fs) => {
 	fs.readdir("./styles/dev", (err, files) => {
-		if(err) { console.log("Reading the directory hit an issue: " + err.stack); }
-		wait = files.length;
+		if(err) { console.log("Reading the styles directory hit an issue: " + err.stack); }
 		files.forEach(file => {
 			var name = file.split("."),
 				devpath = "./styles/dev/" + file,
@@ -27,65 +115,95 @@ exports.minify_all_but_content = (mkdirp, compressor, minify, fs) => {
 					if(err) { console.log("Minifying the css file " + file + " threw an error: " + err.stack); }
 				}
 			});
-			wait--;
-			if(wait == 0) {
-				// Minifies requirejs 
-				new compressor.minify({
-					"type": "yui-js",
-					"fileIn": "./node_modules/requirejs/require.js",
-					"fileOut": "./scripts/dist/require-min.js",
-					"callback": (err, result) => {
-						if(err) { console.log("Minifying the requirejs file threw an error: " + err.stack); }
-						// Minifies the html files in /client
-						container = ["./client/template.html", "./client/about.html", "./client/notation.html"];
-						container.forEach(file => {
-							fs.readFile(file, "utf8", (err, data) => {
-								if(err) { console.log("Could not read the file " + file + ": " + err.stack); }
-								splitting = file.split("/");
-								newpath = "";
-								name = splitting[splitting.length - 1].split(".");
-								for(var k = 0; k < splitting.length - 1; k++) {
-									newpath += splitting[k] + "/";
-								}
-								newpath += "dist/";
-								var data_min = minify(data, {
-									"removeComments": true,
-									"removeCommentsFromCDATA": true,
-									"collapseWhitespace": true,
-									"collapseInlineTagWhitespace": true,
-									"removeAttributeQuotes": true,
-									"removeRedundantAttributes": true,
-									"useShortDoctype": true,
-									"minifyJS": true,
-									"minifyCSS": true
-								});
-								var obj = {
-									"data_min": data_min,
-									"file_name": newpath + name[0] + "-min." + name[1],
-								};
-								tmp.push(obj);
-								if(tmp.length == container.length) {
-									wait = container.length;
-									mkdirp(newpath, err => {
-										if(err) { console.log("Could not make the directory" + newpath + ": " + err.stack); }
-										tmp.forEach(result => {
-											fs.writeFile(result.file_name, result.data_min, err => {
-												if(err) { console.log("Could not write the file " + result.file_name + ": " + err.stack); }
-												wait--;
-												if(wait == 0) {
-													console.log("All html files in /client, CSS files in /styles/dev, and RequireJS have been minified!");
-												}
-											});
-										});
-									});
-								}
-							});
+		});
+	});
+};
+
+// Minifies all necessary js files
+exports.minify_js = (compressor, fs) => {
+	new compressor.minify({
+		"type": "yui-js",
+		"fileIn": "./node_modules/requirejs/require.js",
+		"fileOut": "./scripts/dist/require-min.js",
+		"callback": (err, result) => { if(err) { console.log("Minifying the requirejs file threw an error: " + err.stack); } }
+	});
+	new compressor.minify({
+		"type": "yui-js",
+		"fileIn": "./node_modules/router5/dist/amd/router5.js",
+		"fileOut": "./scripts/dist/router5-min.js",
+		"callback": (err, result) => { if(err) { console.log("Minifying the router5 file threw an error: " + err.stack); } }
+	});
+	new compressor.minify({
+		"type": "yui-js",
+		"fileIn": "./bower_components/materializecss-amd/dist/materialize.amd.js",
+		"fileOut": "./scripts/dist/materialize.amd-min.js",
+		"callback": (err, result) => { if(err) { console.log("Minifying the materialize.amd file threw an error: " + err.stack); } }
+	});
+	// fs.readdir("./scripts/front-end", (err, files) => {
+	// 	if(err) { console.log("Reading the styles directory hit an issue: " + err.stack); }
+	// 	files.forEach(file => {
+	// 		var name = file.split("."),
+	// 			devpath = "./scripts/front-end/" + file,
+	// 			distpath = "./scripts/dist/" + name[0] + "-min.js";
+	// 		new compressor.minify({
+	// 			"type": "yui-js",
+	// 			"fileIn": devpath,
+	// 			"fileOut": distpath,
+	// 			"callback": (err, result) => {
+	// 				if(err) { console.log("Minifying the js file " + file + " threw an error: " + err.stack); }
+	// 			}
+	// 		});
+	// 	});
+	// });
+};
+
+// Minifies the html files in /client
+exports.minify_html = (minify, mkdirp, fs) => {
+	var container = ["./client/template.html", "./client/about.html", "./client/notation.html"],
+		tmp = [];
+	container.forEach(file => {
+		fs.readFile(file, "utf8", (err, data) => {
+			if(err) { console.log("Could not read the file " + file + ": " + err.stack); }
+			splitting = file.split("/");
+			newpath = "";
+			name = splitting[splitting.length - 1].split(".");
+			for(var k = 0; k < splitting.length - 1; k++) {
+				newpath += splitting[k] + "/";
+			}
+			newpath += "dist/";
+			var data_min = minify(data, {
+				"removeComments": true,
+				"removeCommentsFromCDATA": true,
+				"collapseWhitespace": true,
+				"collapseInlineTagWhitespace": true,
+				"removeAttributeQuotes": true,
+				"removeRedundantAttributes": true,
+				"useShortDoctype": true,
+				"minifyJS": true,
+				"minifyCSS": true
+			});
+			var obj = {
+				"data_min": data_min,
+				"file_name": newpath + name[0] + "-min." + name[1],
+			};
+			tmp.push(obj);
+			if(tmp.length == container.length) {
+				wait = container.length;
+				mkdirp(newpath, err => {
+					if(err) { console.log("Could not make the directory" + newpath + ": " + err.stack); }
+					tmp.forEach(result => {
+						fs.writeFile(result.file_name, result.data_min, err => {
+							if(err) { console.log("Could not write the file " + result.file_name + ": " + err.stack); }
+							wait--;
+							if(wait == 0) {
+								console.log("All html files in /client, CSS files in /styles/dev, and RequireJS have been minified!");
+							}
 						});
-					}
+					});
 				});
 			}
 		});
 	});
-}
+};
 
 module.exports = exports;
