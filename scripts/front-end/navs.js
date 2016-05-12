377513780d5b9bd2d8b6db83091e22fd5a63fb2f
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-05-12 02:13:54 -0400

    Front-End Navs
    Minification of Resources & Rewriting Lambda Expressions into Regular
    Function Synthax for Backwards Compatability

--- scripts/front-end/navs.js
+++ scripts/front-end/navs.js
@@ -1,4 +1,4 @@
-define(["app/functions"], function(functions) {
+define(["dist/functions-min"], function(functions) {
 	var exports = {};
 
 	/*
@@ -7,7 +7,7 @@ define(["app/functions"], function(functions) {
 	Adds the neccesary "padding" at the bottom of the menu.
 
 	*/
-	exports.extra = () => {
+	exports.extra = function() {
 		$(".side-nav").append($("<li>").addClass("no-padding extra_li"));
 		// $(".side-nav").append($("<li>").addClass("no-padding extra_li"));
 		// if(window.innerWidth < 992) {
@@ -31,19 +31,19 @@ define(["app/functions"], function(functions) {
 			An array of all the subjects
 
 	*/
-	exports.subject_side_nav = subjects => {
+	exports.subject_side_nav = function(subjects) {
 		var sidenav = $(".side-nav");
 
 		sidenav.empty();
 
-		var results = subjects.map(subject => {
+		var results = subjects.map(function(subject) {
 			var subjectli = $("<li>").addClass("no-padding").attr("id", "subjects_li" + subject.sid),
 				link = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "subjects_" + subject.sid).text(subject.clean_name);
 			link.append($("<i>").addClass("material-icons right").text("arrow_forward"));
 			return subjectli.append(link);
 		});
 
-		results.forEach(subject => { functions.is_mobile() ? sidenav.append(subject, $("<li>").addClass("divider")) : sidenav.append(subject); });
+		results.forEach(function(subject) { functions.is_mobile() ? sidenav.append(subject, $("<li>").addClass("divider")) : sidenav.append(subject); });
 
 		exports.extra();
 	};
@@ -59,7 +59,7 @@ define(["app/functions"], function(functions) {
 			An object representing the current subject
 
 	*/
-	exports.topic_side_nav = subject => {
+	exports.topic_side_nav = function(subject) {
 		var	sidenav = $(".side-nav"),
 			subjectLi = $("<li>").addClass("no-padding").attr("id", "subject_li" + subject.sid),
 			link = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "subjectnav").text("All Subjects");
@@ -70,14 +70,14 @@ define(["app/functions"], function(functions) {
 		sidenav.append(subjectLi);
 		sidenav.append($("<li>").addClass("divider"));
 
-		var results = subject.topics.map(topic => {
+		var results = subject.topics.map(function(topic) {
 			var topicli = $("<li>").addClass("no-padding").attr("id", "topics_li" + topic.tid);
 			link = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "topics_" + topic.tid).text(topic.clean_name);
 			link.append($("<i>").addClass("material-icons right").text("arrow_forward"));
 			return topicli.append(link);
 		});
 
-		results.forEach(topic => { functions.is_mobile() ? sidenav.append(topic, $("<li>").addClass("divider")) : sidenav.append(topic); });
+		results.forEach(function(topic) { functions.is_mobile() ? sidenav.append(topic, $("<li>").addClass("divider")) : sidenav.append(topic); });
 
 		exports.extra();
 	};
@@ -95,7 +95,7 @@ define(["app/functions"], function(functions) {
 			An object representing the current topic
 
 	*/
-	exports.section_side_nav = (topic, subject) => {
+	exports.section_side_nav = function(topic, subject) {
 		var sidenav = $(".side-nav"),
 			topicli = $("<li>").addClass("no-padding").attr("id", "topic_li" + topic.tid),
 			topicnav = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "topicnav_" + topic.tid).text(subject.clean_name);
@@ -105,14 +105,14 @@ define(["app/functions"], function(functions) {
 		topicli.append(topicnav);
 		sidenav.append(topicli, $("<li>").addClass("divider"));
 
-		var results = topic.sections.map(section => {
+		var results = topic.sections.map(function(section) {
 			var sectionli = $("<li>").addClass("no-padding").attr("id", "sections_li" + section.section_id),
 				link = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "sections_" + section.section_id).text(section.clean_name);
 			link.append($("<i>").addClass("material-icons right").text("arrow_forward"));
 			return sectionli.append(link);
 		});
 
-		results.forEach(section => { functions.is_mobile() ? sidenav.append(section, $("<li>").addClass("divider")) : sidenav.append(section); })
+		results.forEach(function(section) { functions.is_mobile() ? sidenav.append(section, $("<li>").addClass("divider")) : sidenav.append(section); })
 
 		exports.extra();
 	};
@@ -130,7 +130,7 @@ define(["app/functions"], function(functions) {
 			An object representing the current section
 
 	*/
-	exports.example_side_nav = (section, topic) => {
+	exports.example_side_nav = function(section, topic) {
 		var sidenav = $(".side-nav"),
 			link = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "sectionname_" + section.section_id).text("Notes"),
 			sectionli = $("<li>").addClass("no-padding").attr("id", "section_li" + section.section_id),
@@ -145,13 +145,13 @@ define(["app/functions"], function(functions) {
 		sectionname.append(link);
 		functions.is_mobile() ? sidenav.append(sectionname, $("<li>").addClass("divider")) : sidenav.append(sectionname);
 		
-		var results = section.examples.map(example => {
+		var results = section.examples.map(function(example) {
 			var exampleli = $("<li>").addClass("no-padding").attr("id", "examples_li" + example.eid);
 			link = $("<a>").addClass("collapsible-header bold menu_items").attr("id", "examples_" + example.eid).text(example.clean_name);
 			return exampleli.append(link);
 		});
 
-		results.forEach(example => { functions.is_mobile() ? sidenav.append(example, $("<li>").addClass("divider")) : sidenav.append(example); })
+		results.forEach(function(example) { functions.is_mobile() ? sidenav.append(example, $("<li>").addClass("divider")) : sidenav.append(example); })
 
 		exports.extra();
 	};
@@ -168,7 +168,7 @@ define(["app/functions"], function(functions) {
 			An object representing "subject" or "topic"
 
 	*/
-	exports.driver = (page, param1, param2) => {
+	exports.driver = function(page, param1, param2) {
 		if(page == "about") { exports.subject_side_nav(param1); }
 		else if(page == "subject") { exports.topic_side_nav(param1); }
 		else {
