0057dd5ca5030a26ed7b29ac305257773a19efe7
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-08-21 10:56:14 -0700

    Adding Box Issue
    Handled the case of where a new box is added and the document height is unchanged.

--- scripts/front-end/functions.js
+++ scripts/front-end/functions.js
@@ -1,6 +1,33 @@
 define(function() {
 	var exports = {};
 
+	exports.scale_down = function(dataUrl, newWidth, imageType, imageArguments) {
+	    "use strict";
+	    var image, oldWidth, oldHeight, newHeight, canvas, ctx, newDataUrl;
+
+	    // Provide default values
+	    imageType = imageType || "image/jpeg";
+	    imageArguments = imageArguments || 0.7;
+
+	    // Create a temporary image so that we can compute the height of the downscaled image.
+	    image = new Image();
+	    image.src = dataUrl;
+	    oldWidth = image.width;
+	    oldHeight = image.height;
+	    newHeight = Math.floor(oldHeight / oldWidth * newWidth)
+
+	    // Create a temporary canvas to draw the downscaled image on.
+	    canvas = document.createElement("canvas");
+	    canvas.width = newWidth;
+	    canvas.height = newHeight;
+
+	    // Draw the downscaled image on the canvas and return the new data URL.
+	    ctx = canvas.getContext("2d");
+	    ctx.drawImage(image, 0, 0, newWidth, newHeight);
+	    newDataUrl = canvas.toDataURL(imageType, imageArguments);
+	    return newDataUrl;
+	};
+
 	/*
 
 	Purpose:
@@ -2590,6 +2617,10 @@ define(function() {
 				data.title_cms[ref] += "_hidden";
 			}
 		});
+		$(".solution_toggle").tooltip("destroy");
+		$(".del-box-tooltipped").tooltip("destroy");
+		$(".add-math-tooltipped").tooltip("destroy");
+		$(".add-image-tooltipped").tooltip("destroy");
 		$(".solution_toggle").tooltip();
 		$(".del-box-tooltipped").tooltip();
 		$(".add-math-tooltipped").tooltip();
@@ -2610,8 +2641,118 @@ define(function() {
 				var file = $("#file")[0].files[0],
 					reader  = new FileReader();
 				reader.addEventListener("load", function () {
+					// var ref = reader.result.indexOf("data:image/jpeg;base64")
+
+
+
+					// returns a function that calculates lanczos weight
+					// function lanczosCreate(lobes) {
+					//     return function(x) {
+					//         if (x > lobes)
+					//             return 0;
+					//         x *= Math.PI;
+					//         if (Math.abs(x) < 1e-16)
+					//             return 1;
+					//         var xx = x / lobes;
+					//         return Math.sin(x) * Math.sin(xx) / x / xx;
+					//     };
+					// }
+
+					// // elem: canvas element, img: image element, sx: scaled width, lobes: kernel radius
+					// function thumbnailer(elem, img, sx, lobes) {
+					//     this.canvas = elem;
+					//     elem.width = img.width;
+					//     elem.height = img.height;
+					//     elem.style.display = "none";
+					//     this.ctx = elem.getContext("2d");
+					//     this.ctx.drawImage(img, 0, 0);
+					//     this.img = img;
+					//     this.src = this.ctx.getImageData(0, 0, img.width, img.height);
+					//     this.dest = {
+					//         width : sx,
+					//         height : Math.round(img.height * sx / img.width),
+					//     };
+					//     this.dest.data = new Array(this.dest.width * this.dest.height * 3);
+					//     this.lanczos = lanczosCreate(lobes);
+					//     this.ratio = img.width / sx;
+					//     this.rcp_ratio = 2 / this.ratio;
+					//     this.range2 = Math.ceil(this.ratio * lobes / 2);
+					//     this.cacheLanc = {};
+					//     this.center = {};
+					//     this.icenter = {};
+					//     setTimeout(this.process1, 0, this, 0);
+					// }
+
+					// thumbnailer.prototype.process1 = function(self, u) {
+					//     self.center.x = (u + 0.5) * self.ratio;
+					//     self.icenter.x = Math.floor(self.center.x);
+					//     for (var v = 0; v < self.dest.height; v++) {
+					//         self.center.y = (v + 0.5) * self.ratio;
+					//         self.icenter.y = Math.floor(self.center.y);
+					//         var a, r, g, b;
+					//         a = r = g = b = 0;
+					//         for (var i = self.icenter.x - self.range2; i <= self.icenter.x + self.range2; i++) {
+					//             if (i < 0 || i >= self.src.width)
+					//                 continue;
+					//             var f_x = Math.floor(1000 * Math.abs(i - self.center.x));
+					//             if (!self.cacheLanc[f_x])
+					//                 self.cacheLanc[f_x] = {};
+					//             for (var j = self.icenter.y - self.range2; j <= self.icenter.y + self.range2; j++) {
+					//                 if (j < 0 || j >= self.src.height)
+					//                     continue;
+					//                 var f_y = Math.floor(1000 * Math.abs(j - self.center.y));
+					//                 if (self.cacheLanc[f_x][f_y] == undefined)
+					//                     self.cacheLanc[f_x][f_y] = self.lanczos(Math.sqrt(Math.pow(f_x * self.rcp_ratio, 2)
+					//                             + Math.pow(f_y * self.rcp_ratio, 2)) / 1000);
+					//                 weight = self.cacheLanc[f_x][f_y];
+					//                 if (weight > 0) {
+					//                     var idx = (j * self.src.width + i) * 4;
+					//                     a += weight;
+					//                     r += weight * self.src.data[idx];
+					//                     g += weight * self.src.data[idx + 1];
+					//                     b += weight * self.src.data[idx + 2];
+					//                 }
+					//             }
+					//         }
+					//         var idx = (v * self.dest.width + u) * 3;
+					//         self.dest.data[idx] = r / a;
+					//         self.dest.data[idx + 1] = g / a;
+					//         self.dest.data[idx + 2] = b / a;
+					//     }
+
+					//     if (++u < self.dest.width)
+					//         setTimeout(self.process1, 0, self, u);
+					//     else
+					//         setTimeout(self.process2, 0, self);
+					// };
+					// thumbnailer.prototype.process2 = function(self) {
+					//     self.canvas.width = self.dest.width;
+					//     self.canvas.height = self.dest.height;
+					//     self.ctx.drawImage(self.img, 0, 0, self.dest.width, self.dest.height);
+					//     self.src = self.ctx.getImageData(0, 0, self.dest.width, self.dest.height);
+					//     var idx, idx2;
+					//     for (var i = 0; i < self.dest.width; i++) {
+					//         for (var j = 0; j < self.dest.height; j++) {
+					//             idx = (j * self.dest.width + i) * 3;
+					//             idx2 = (j * self.dest.width + i) * 4;
+					//             self.src.data[idx2] = self.dest.data[idx];
+					//             self.src.data[idx2 + 1] = self.dest.data[idx + 1];
+					//             self.src.data[idx2 + 2] = self.dest.data[idx + 2];
+					//         }
+					//     }
+					//     self.ctx.putImageData(self.src, 0, 0);
+					//     self.canvas.style.display = "block";
+					// };
+
+					// var canvas = document.createElement("canvas");
+    				// new thumbnailer(canvas, reader.result, 188, 3); //this produces lanczos3
+
+
 			  		obj.append($("<div>").addClass("latex_equation")
 						.append($("<img>").attr("src", reader.result)));
+						// .append($("<img>").attr("src", canvas.toDataURL())));
+			  		// console.log(reader.result);
+			  		// console.log(exports.scale_down(reader.result.substring(23), 500, "image/jpeg", "base64"));
 					$("#file").remove();
 		  		}, false);
 			  	if(file) {
@@ -3013,6 +3154,11 @@ define(function() {
 							$(".latex_body").attr("contentEditable", "true");
 							exports.handle_button(1);
 							exports.latex_cms_links(data);
+
+							document.height = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+                      			document.documentElement.clientHeight, document.documentElement.scrollHeight, 
+                      			document.documentElement.offsetHeight );
+
 						});
 						exports.latex_cms_links(data);
 					}
