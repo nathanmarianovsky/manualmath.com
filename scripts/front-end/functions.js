e2123e2782c0bdc180f7e86d8d0edcbed6e3eb04
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-08-21 22:02:52 -0700

    Rescaled Image Uploads
    Images are not set to be 500x500.

--- scripts/front-end/functions.js
+++ scripts/front-end/functions.js
@@ -2645,105 +2645,30 @@ define(function() {
 
 
 
-					/**
-					 * Hermite resize - fast image resize/resample using Hermite filter. 1 cpu version!
-					 * 
-					 * @param {HtmlElement} canvas
-					 * @param {int} width
-					 * @param {int} height
-					 * @param {boolean} resize_canvas if true, canvas will be resized. Optional.
-					 */
-					function resample_single(canvas, width, height, resize_canvas) {
-					    var width_source = canvas.width;
-					    var height_source = canvas.height;
-					    width = Math.round(width);
-					    height = Math.round(height);
+					function resizeImage(url, width, height, callback) {
+					    var sourceImage = new Image();
 
-					    var ratio_w = width_source / width;
-					    var ratio_h = height_source / height;
-					    var ratio_w_half = Math.ceil(ratio_w / 2);
-					    var ratio_h_half = Math.ceil(ratio_h / 2);
-
-					    var ctx = canvas.getContext("2d");
-					    var img = ctx.getImageData(0, 0, width_source, height_source);
-					    var img2 = ctx.createImageData(width, height);
-					    var data = img.data;
-					    var data2 = img2.data;
-
-					    for (var j = 0; j < height; j++) {
-					        for (var i = 0; i < width; i++) {
-					            var x2 = (i + j * width) * 4;
-					            var weight = 0;
-					            var weights = 0;
-					            var weights_alpha = 0;
-					            var gx_r = 0;
-					            var gx_g = 0;
-					            var gx_b = 0;
-					            var gx_a = 0;
-					            var center_y = (j + 0.5) * ratio_h;
-					            var yy_start = Math.floor(j * ratio_h);
-					            var yy_stop = Math.ceil((j + 1) * ratio_h);
-					            for (var yy = yy_start; yy < yy_stop; yy++) {
-					                var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
-					                var center_x = (i + 0.5) * ratio_w;
-					                var w0 = dy * dy; //pre-calc part of w
-					                var xx_start = Math.floor(i * ratio_w);
-					                var xx_stop = Math.ceil((i + 1) * ratio_w);
-					                for (var xx = xx_start; xx < xx_stop; xx++) {
-					                    var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
-					                    var w = Math.sqrt(w0 + dx * dx);
-					                    if (w >= 1) {
-					                        //pixel too far
-					                        continue;
-					                    }
-					                    //hermite filter
-					                    weight = 2 * w * w * w - 3 * w * w + 1;
-					                    var pos_x = 4 * (xx + yy * width_source);
-					                    //alpha
-					                    gx_a += weight * data[pos_x + 3];
-					                    weights_alpha += weight;
-					                    //colors
-					                    if (data[pos_x + 3] < 255)
-					                        weight = weight * data[pos_x + 3] / 250;
-					                    gx_r += weight * data[pos_x];
-					                    gx_g += weight * data[pos_x + 1];
-					                    gx_b += weight * data[pos_x + 2];
-					                    weights += weight;
-					                }
-					            }
-					            data2[x2] = gx_r / weights;
-					            data2[x2 + 1] = gx_g / weights;
-					            data2[x2 + 2] = gx_b / weights;
-					            data2[x2 + 3] = gx_a / weights_alpha;
-					        }
-					    }
-					    //clear and resize canvas
-					    if (resize_canvas === true) {
+					    sourceImage.onload = function() {
+					        // Create a canvas with the desired dimensions
+					        var canvas = document.createElement("canvas");
 					        canvas.width = width;
 					        canvas.height = height;
-					    } else {
-					        ctx.clearRect(0, 0, width_source, height_source);
+
+					        // Scale and draw the source image to the canvas
+					        canvas.getContext("2d").drawImage(sourceImage, 0, 0, width, height);
+
+					        // Convert the canvas to a data URL in PNG format
+					        callback(canvas.toDataURL());
 					    }
 
-					    //draw
-					    ctx.putImageData(img2, 0, 0);
+					    sourceImage.src = url;
 					}
 
-					// var canvas = $("<canvas>").append($("<img>").attr("src", reader.result));
-					var img = new Image();
-					var canvas = document.createElement("canvas");
-					var ctx = canvas.getContext("2d");
-					img.src = reader.result;
-					ctx.clearRect(0, 0, 1000, 1000);
-					img.onload = function() {
-						ctx.drawImage(img, 0, 0);
-	    				// resample_single(canvas, 650, 650, 1);
-					}
-
-
-			  		obj.append($("<div>").addClass("latex_equation")
-						.append(canvas));
-						// .append($("<img>").attr("src", canvas.toDataURL())));
+					resizeImage(reader.result, 500, 500, function(compressed_data) {
+				  		obj.append($("<div>").addClass("latex_equation")
+							.append($("<img>").attr("src", compressed_data)));
+					});
+						// .append(canvas));
 			  		// console.log(reader.result);
 			  		// console.log(exports.scale_down(reader.result.substring(23), 500, "image/jpeg", "base64"));
 					$("#file").remove();
